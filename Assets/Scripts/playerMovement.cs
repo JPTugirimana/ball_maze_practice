4bf5985d25aad69d6d83a3a83c5798835aca70e3
audreyandoy <audreylandoy@gmail.com>
2020-06-23 17:00:12 -0700

    finished project example

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerMovement : MonoBehaviour
+{
+    public float velocity = 10f;
+    public Rigidbody rb;
+    private int score;
+    public Text scoreText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        setScoreText();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * velocity);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            score += 1;
+            setScoreText();
+            print("Score: " + score);
+        }
+    }
+
+    void setScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+}
